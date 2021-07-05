ec64aa09beb490c35830fa841c896a0d5796a1f6
hironomiu <hironomiu@gmail.com>
2021-07-05 16:16:40 +0900

    commited

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# next-sample
+# next-vercel-sample
 
 ## Vercel
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Layout from "../components/Layout"
+import { useEffect, useState } from "react"
+import { resolveHref } from "next/dist/next-server/lib/router/router"
+
 export default function Home() {
+  const [name, setName] = useState("hello")
+  const func = async () => {
+    const rest = await fetch("/api/hoge")
+    const posts = await rest.json()
+    return posts.name
+  }
+
+  useEffect(() => {
+    func().then((data) => {
+      console.log(data)
+      setName(data)
+    })
+  }, [])
+
   return (
     <Layout>
-      <p>hello next-sample!!!!!</p>
+      <p>{name}</p>
     </Layout>
   )
 }
