9ae9c2b45c1cbd0d59ece3ebda6bfc1a0d364e04
hironomiu <hironomiu@gmail.com>
2021-07-15 15:17:03 +0900

    commited

--- pages/api/hoge.js
+++ pages/api/hoge.js
@@ -12,13 +12,4 @@ export default (req, res) => {
       { id: 6, name: "baby" },
     ])
   }, time)
-
-  // res.status(200).json([
-  //   { id: 1, name: "hoge" },
-  //   { id: 2, name: "fuga" },
-  //   { id: 3, name: "piyo" },
-  //   { id: 4, name: "jojo" },
-  //   { id: 5, name: "acho" },
-  //   { id: 6, name: "baby" },
-  // ])
 }
