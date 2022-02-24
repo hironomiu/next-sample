986ec73c79fafdb3efda3ea1747a70c8aafc01e0
hironomiu <hironomiu@gmail.com>
2022-02-25 07:36:39 +0900

    commited

--- pages/users.tsx
+++ pages/users.tsx
@@ -2,25 +2,26 @@ import Layout from '../components/Layout'
 import { GetStaticProps } from 'next'
 
 type POST = {
+  userId: number
   id: number
-  name: string
+  title: string
+  body: string
 }
 
 interface STATICPROPS {
   posts: POST[]
 }
 
-// const Users: React.FC<STATICPROPS> = (props) => {
-const Users: React.FC = () => {
+const Users: React.FC<STATICPROPS> = (props) => {
+  // const Users: React.FC = () => {
   return (
     <Layout>
-      {/* <h1 className="text-2xl pb-10">Users</h1>
+      <h1 className="text-2xl pb-10">Users</h1>
       {props.posts.map((line: POST, index) => (
-        <p key={index}>
-          id:{line.id},name:{line.name}
+        <p key={line.id}>
+          userId:{line.userId},id:{line.id},title:{line.title}
         </p>
-      ))} */}
-      User
+      ))}
     </Layout>
   )
 }
@@ -28,11 +29,14 @@ const Users: React.FC = () => {
 // TODO Build時に自身のAPIは叩けないのでエラーとなってる可能性がある
 
 export const getStaticProps: GetStaticProps = async () => {
-  const url = process.env.VERCEL_URL
-    ? 'https://' + process.env.VERCEL_URL
-    : 'http://localhost:3003'
-  const response = await fetch(new URL(url + '/api/users').toString())
+  // const url = process.env.VERCEL_URL
+  //   ? 'https://' + process.env.VERCEL_URL
+  //   : // : 'http://localhost:3003'
+  //     'https://jsonplaceholder.typicode.com/users?_limit=10'
+  const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
+  const response = await fetch(new URL(url).toString())
   const posts = await response.json()
+  console.log(posts)
   return {
     props: { posts },
   }
