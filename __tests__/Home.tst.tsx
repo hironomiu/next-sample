ccf8b6766b31589e3123e77e00e28ba9c5215200
hironomiu <hironomiu@gmail.com>
2022-02-26 08:31:51 +0900

    commited

--- __tests__/Home.tst.tsx
+++ __tests__/Home.tst.tsx
@@ -2,11 +2,9 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
-describe('', () => {
-  it('', async () => {
+describe('Homeの表示テスト', () => {
+  it('Home Pageが表示されていること', async () => {
     render(<Home />)
-
     expect(await screen.findByText('Home Page')).toBeInTheDocument()
-    screen.debug()
   })
 })
