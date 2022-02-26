9e46ae6d5fef07497af1235494b65227085d146d
hironomiu <hironomiu@gmail.com>
2022-02-26 13:17:33 +0900

    commited

---
+++ __tests__/TabA.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import TabA from '../pages/tab-a'
+
+describe('', () => {
+  it('', async () => {
+    render(<TabA />)
+    expect(await screen.getByText('TabA Page')).toBeInTheDocument()
+  })
+})
--- pages/tab-a.tsx
+++ pages/tab-a.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const TabA = () => {
   return (
     <Layout>
-      <h1 className="text-2xl">TabA</h1>
+      <h1 className="text-2xl">TabA Page</h1>
     </Layout>
   )
 }
