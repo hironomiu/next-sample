406872d0686763c2a68b627d26e3797a5f56525a
hironomiu <hironomiu@gmail.com>
2022-03-03 18:04:42 +0900

    commited

--- __tests__/Counter.test.tsx
+++ __tests__/Counter.test.tsx
@@ -10,4 +10,10 @@ describe('Counter', () => {
     userEvent.click(screen.getByTestId('decrement-button'))
     expect(screen.getByTestId('counter').textContent).toBe('-1')
   })
+  it('increment', () => {
+    render(<Counter />)
+    expect(screen.getByTestId('counter').textContent).toBe('0')
+    userEvent.click(screen.getByTestId('increment-button'))
+    expect(screen.getByTestId('counter').textContent).toBe('1')
+  })
 })
--- pages/counter.tsx
+++ pages/counter.tsx
@@ -5,16 +5,33 @@ const Counter = () => {
   const handleDecrement = () => {
     setCounter((prevCounter) => (prevCounter = prevCounter - 1))
   }
+  const handleIncrement = () => {
+    setCounter((prevCounter) => (prevCounter = prevCounter + 1))
+  }
 
   return (
     <Layout>
-      <h1 className="text-2xl">Counter Page</h1>
-      <div className="flex flex-row">
-        <button onClick={handleDecrement} data-testid="decrement-button">
-          -
-        </button>
-        <span data-testid="counter">{counter}</span>
-        <button>+</button>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl">Counter Page</h1>
+        <div className="flex flex-row justify-center items-center">
+          <button
+            className="text-2xl"
+            onClick={handleDecrement}
+            data-testid="decrement-button"
+          >
+            -
+          </button>
+          <span className="text-2xl mx-4" data-testid="counter">
+            {counter}
+          </span>
+          <button
+            className="text-2xl"
+            onClick={handleIncrement}
+            data-testid="increment-button"
+          >
+            +
+          </button>
+        </div>
       </div>
     </Layout>
   )
