dc662e978a94c1694896bb2c72f1ac5702243a46
hironomiu <hironomiu@gmail.com>
2022-03-05 17:53:06 +0900

    commited

--- __tests__/TabA.test.tsx
+++ __tests__/TabA.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import TabA from '../pages/tab-a'
 
-describe('', () => {
+describe('TabA', () => {
   it('', async () => {
     render(<TabA />)
     expect(await screen.findByText('TabA Page')).toBeInTheDocument()
