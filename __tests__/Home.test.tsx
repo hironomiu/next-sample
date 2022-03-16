e5e199b2feed1cc89cbebe02be7db382a488f3da
hironomiu <hironomiu@gmail.com>
2022-03-16 18:31:44 +0900

    commited

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -3,16 +3,8 @@ import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 import userEvent from '@testing-library/user-event'
 import { getPage, initTestHelpers } from 'next-page-tester'
-import Link from 'next/link'
 
 initTestHelpers()
-
-jest.mock(
-  'next/link',
-  () =>
-    ({ children }: any) =>
-      children
-)
 
 describe('Homeの表示テスト', () => {
   it('Home Pageが表示されていること', async () => {
@@ -26,10 +18,9 @@ describe('Homeの表示テスト', () => {
     render(page)
 
     expect(await screen.findByText('Home Page')).toBeInTheDocument()
-    // clickまでは通るがscreen.debug()で確認すると画面遷移はしておらずエラーになる
+    userEvent.click(screen.getByTestId('posts-nav'))
 
-    // userEvent.click(screen.getByTestId('posts-nav'))
-
-    // expect(await screen.findByText('Posts Page')).toBeInTheDocument()
+    // TODO 画面が遷移できていない
+    expect(await screen.findByText('Posts Page')).toBeInTheDocument()
   })
 })
