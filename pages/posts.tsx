3e1138351d45922b1a3b100263e8d1fbfb6c2436
hironomiu <hironomiu@gmail.com>
2022-03-22 08:19:16 +0900

    commited

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -4,7 +4,6 @@ import { POST } from '../types'
 import PostLine from '../components/parts/PostLine'
 
 const Posts: React.FC<{ posts: POST[] }> = ({ posts }) => {
-  // console.log('posts called')
   return (
     <Layout>
       <h1 className="text-2xl pb-10">Posts Page</h1>
@@ -18,11 +17,13 @@ const Posts: React.FC<{ posts: POST[] }> = ({ posts }) => {
 // TODO Build時に自身のAPIは叩けないのでエラーとなってる可能性がある
 // 一旦jsonplaceholderからデータを取得に変更
 
+// SSG
 export const getStaticProps: GetStaticProps = async () => {
   // const url = process.env.VERCEL_URL
   //   ? 'https://' + process.env.VERCEL_URL
   //   : // : 'http://localhost:3003'
   //     'https://jsonplaceholder.typicode.com/users?_limit=10'
+  console.log('called')
   const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
   const response = await fetch(new URL(url).toString())
   const posts = await response.json()
