c787c248a8580ba0ebb596f62932dafa2e4a5d8d
hironomiu <hironomiu@gmail.com>
2022-03-26 09:34:57 +0900

    commited

--- __tests__/TabA.test.tsx
+++ __tests__/TabA.test.tsx
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import TabA from '../pages/tab-a'
 
 describe('TabA', () => {
-  it('', async () => {
+  it('render & input test cases', async () => {
     render(<TabA />)
     expect(await screen.findByText('TabA Page')).toBeInTheDocument()
+    userEvent.type(screen.getByTestId('input'), 'abcd')
+    expect(await screen.findByText('abcd')).toBeInTheDocument()
   })
 })
--- pages/tab-a.tsx
+++ pages/tab-a.tsx
@@ -1,9 +1,24 @@
 import Layout from '../components/Layout'
+import { useState } from 'react'
+import { handleRequest } from 'msw'
 
 const TabA = () => {
+  const [input, setInput] = useState('')
+  const handleChange = (e) => {
+    // setInput((_prev) => (_prev = e.target.value))
+    setInput(e.target.value)
+  }
   return (
     <Layout>
       <h1 className="text-2xl">TabA Page</h1>
+      <span>{input}</span>
+      <input
+        type="text"
+        value={input}
+        onChange={handleChange}
+        className="border"
+        data-testid="input"
+      />
     </Layout>
   )
 }
